2c51bbcf6992cedcbaeb65a34bca9fe609d90f16
LeeBoy <jobboy19890101@gmail.com>
2019-08-01 12:42:21 +0800

    Addition Rule and Monty Hall

---
+++ Probability/1-3-2.R
@@ -0,0 +1,13 @@
+# The variable `B` specifies the number of times we want the simulation to run. Let's run the Monte Carlo simulation 10,000 times.
+B <- 10000
+
+# Use the `set.seed` function to make sure your answer matches the expected result after random sampling.
+set.seed(1)
+
+# Create an object called `results` that replicates for `B` iterations a simulated series and determines whether that series contains at least four wins for the Cavs.
+results <- replicate(B, {
+  result <- sample(0:1, 6, replace = TRUE)
+  sum(result) >= 4
+})
+
+mean(results)
